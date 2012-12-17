c7047b06995fa871053c3f17dfdfcdc6cf1f5e06
Angela Prestaux <angela.prestaux@gmail.com>
2012-12-17 10:46:20 +0100

    MongoDB persistence (not working)

--- src/main/java/com/github/funreco/domain/Action.java
+++ src/main/java/com/github/funreco/domain/Action.java
@@ -2,10 +2,17 @@ package com.github.funreco.domain;
 
 import java.util.Date;
 
+import org.bson.types.ObjectId;
+
+@com.google.code.morphia.annotations.Entity(value = "funreco", noClassnameStored = true)
 public class Action {
+	@com.google.code.morphia.annotations.Id
+    private ObjectId id;
 	
 	private Profile profile;
 	private Object object;
+	
+	@Column
 	private Date date = new Date();
 
 }
---
+++ src/main/java/com/github/funreco/service/SpringConfig.java
@@ -0,0 +1,22 @@
+package com.github.funreco.service;
+
+import java.net.UnknownHostException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.github.funreco.domain.Action;
+import com.google.code.morphia.Morphia;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public Datastore datastore() throws MongoException, UnknownHostException {
+        Morphia morphia = new Morphia();
+
+        morphia.map(Action.class);
+
+        return morphia.createDatastore(new Mongo(), "funreco");
+    }
+}
