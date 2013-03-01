3c314e7330b13aedee70b27e6689b4b8d7e48ae0
Damien Bourdette <damien.bourdette@rtlnet.fr>
2013-03-01 10:44:26 +0100

    better usage of mockito (@RunWith(MockitoJUnitRunner.class) @Mock @InjectMocks)

--- src/test/java/com/github/aprestaux/funreco/web/controller/RestControllerTest.java
+++ src/test/java/com/github/aprestaux/funreco/web/controller/RestControllerTest.java
@@ -1,8 +1,10 @@
 package com.github.aprestaux.funreco.web.controller;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.github.aprestaux.funreco.api.Attributes;
 import com.github.aprestaux.funreco.service.ProfileNotFoundException;
@@ -11,18 +13,15 @@ import com.github.aprestaux.funreco.utils.TestData;
 
 import static com.github.aprestaux.funreco.utils.TestData.testProfileAttributes;
 import static org.fest.assertions.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class RestControllerTest {
+    @Mock
+    private RecommendationFacade facade;
+
+    @InjectMocks
     private RestController restController = new RestController();
-
-    private RecommendationFacade facade = mock(RecommendationFacade.class);
-
-    @Before
-    public void setup() {
-        ReflectionTestUtils.setField(restController, "recommendationFacade", facade);
-    }
 
     @Test
     public void getProfile() throws ProfileNotFoundException {
